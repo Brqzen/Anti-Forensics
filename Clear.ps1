73bf00a17bf4b87c67bb63998fbea504f036054e
Bruzen <97251019+Brqzen@users.noreply.github.com>
2025-03-25 09:34:28 -0500

    Update Clear.ps1

--- Clear.ps1
+++ Clear.ps1
@@ -1,6 +1,12 @@
 Write-Host "Clearing Registry"
 reg delete "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store" /f
 reg delete "HKCR\Local Settings\Software\Microsoft\Windows\Shell" /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage" /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32" /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist" /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" /f
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /f
 
 
 taskkill /f /im explorer.exe
@@ -26,6 +32,7 @@ foreach ($drive in $drives) {
     Write-Host "Deleting USN journal on $driveLetter"
     fsutil usn deleteJournal /d $driveLetter
 }
+
 
 Start-Process explorer.exe
 net start EventLog
