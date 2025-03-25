32349ca3407043d723983596c411c048496b6e91
Bruzen <97251019+Brqzen@users.noreply.github.com>
2025-03-25 08:36:21 -0500

    Update Clear.ps1

--- Clear.ps1
+++ Clear.ps1
@@ -1,19 +1,20 @@
+Write-Host "Clearing Registry"
 reg delete "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store" /f
 reg delete "HKCR\Local Settings\Software\Microsoft\Windows\Shell" /f
 
 taskkill /f /im explorer.exe
 net stop EventLog
 
+Write-Host "Clearing Files"
 Remove-Item "C:\Windows\Prefetch\*.pf"
 Remove-Item "C:\Windows\System32\winevt\Logs\*.evtx"
-Remove-Item "C:\Windows\appcompat\Programs\*"
 Remove-Item "C:\Windows\appcompat\pca\*.txt"
 Remove-Item "C:\Windows\System32\sru\SRUDB.dat"
 Remove-Item "C:\ProgramData\NVIDIA Corporation\Drs\nvAppTimestamps"
 Remove-Item "env:%temp%\*"
 Remove-Item "env:%AppData%\Microsoft\Windows\Recent\*.lnk"
 Remove-Item "env:%AppData%\Microsoft\Windows\Recent\CustomDestinations\*ms"
-Remove-Item "env:%APPDATA%\Microsoft\Windows\Recent\AutomaticDestinations\*ms"
+Remove-Item "env:%AppData%\Microsoft\Windows\Recent\AutomaticDestinations\*ms"
 Remove-Item "env:%AppData%\Microsoft\Windows\PowerShell\PSReadLine\*.txt"
 
 
