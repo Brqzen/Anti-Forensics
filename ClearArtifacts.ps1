2ffc0f6b7187d2f3dcdbaab524c3fcc2209b9a64
Bruzen <97251019+Brqzen@users.noreply.github.com>
2025-03-26 11:32:29 -0500

    Update ClearArtifacts.ps1

--- ClearArtifacts.ps1
+++ ClearArtifacts.ps1
@@ -1,30 +1,45 @@
-Write-Host "Clearing Registry"
+Write-Host "Clearing Compatibility Store"
 reg delete "HKCU\Software\Microsoft\Windows NT\CurrentVersion\AppCompatFlags\Compatibility Assistant\Store" /f
+Write-Host "Clearing MuiCache"
 reg delete "HKCR\Local Settings\Software\Microsoft\Windows\Shell" /f
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\FeatureUsage" /f
+Write-Host "Clearing ComDlg32"
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\ComDlg32" /f
+Write-Host "Clearing RecentDocs"
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RecentDocs" /f
+Write-Host "Clearing UserAssist"
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\UserAssist" /f
+Write-Host "Clearing Taskband"
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband" /f
+Write-Host "Clearing RunMRU"
 reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU" /f
 
 
 taskkill /f /im explorer.exe
 net stop EventLog
 
-Write-Host "Clearing Files"
+
+Write-Host "Clearing Prefetch"
 Remove-Item "C:\Windows\Prefetch\*.pf" -Force
+Write-Host "Clearing Event Logs"
 Remove-Item "C:\Windows\System32\winevt\Logs\*.evtx" -Force
+Write-Host "Clearing pca"
 Remove-Item "C:\Windows\appcompat\pca\*.txt" -Force
+Write-Host "Clearing SRUM"
 Remove-Item "C:\Windows\System32\sru\SRUDB.dat" -Force
+Write-Host "Clearing NvAppTimestamps"
 Remove-Item "C:\ProgramData\NVIDIA Corporation\Drs\nvAppTimestamps" -Force
+Write-Host "Clearing Temp"
 Remove-Item "$env:temp\*" -Force
+Write-Host "Clearing Recent Files"
 Remove-Item "$env:AppData\Microsoft\Windows\Recent\*.lnk" -Force
 Remove-Item "$env:AppData\Microsoft\Windows\Recent\CustomDestinations\*ms" -Force
 Remove-Item "$env:AppData\Microsoft\Windows\Recent\AutomaticDestinations\*ms" -Force
+Write-Host "Clearing PSReadLine"
 Remove-Item "$env:AppData\Microsoft\Windows\PowerShell\PSReadLine\*.txt" -Force
 
 
+Write-Host "Clearing ShimCache"
 Rundll32.exe apphelp.dll,ShimFlushCache
 $drives = Get-WmiObject Win32_LogicalDisk | Where-Object { $_.DriveType -eq 3 }
 foreach ($drive in $drives) {
