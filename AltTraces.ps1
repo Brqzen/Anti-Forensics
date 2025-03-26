67d59f6bc515e9ae06b963f90a87fe9df6f24276
Bruzen <97251019+Brqzen@users.noreply.github.com>
2025-03-26 18:08:20 -0500

    Update AltTraces.ps1

--- AltTraces.ps1
+++ AltTraces.ps1
@@ -1,9 +1,22 @@
+Write-Host "Please fully close all Minecraft Launchers, AnyDesk, and Discord before continuing."
+pause
+
+Write-Host "Clearing Discord Traces"
 Remove-Item "C:\Users\%username%\AppData\Roaming\discord\Local Storage\leveldb\*.ldb" -Force
+
+Write-Host "Clearing AnyDesk Traces"
 Remove-Item "$env:AppData\AnyDesk" -Force
+
+Write-Host "Clearing .minecraft Traces"
 Remove-Item "$env:AppData\.minecraft\user*" -Force
 Remove-Item "$env:AppData\.minecraft\launcher*" -Force
 Remove-Item "$env:AppData\.minecraft\logs" -Force
 Remove-Item "$env:AppData\.minecraft\clientId.txt" -Force
+
+Write-Host "Clearing Badlion Traces"
 Remove-Item "$env:AppData\Badlion Client" -Force
+
+Write-Host "Clearing Lunar Traces"
 Remove-Item "$env:AppData\lunarclient" -Force
+
 pause
