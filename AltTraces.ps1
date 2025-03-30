61c48e1371ff6a4828d9841576d93f277eab6824
Bruzen <97251019+Brqzen@users.noreply.github.com>
2025-03-30 15:54:55 -0500

    Update AltTraces.ps1

--- AltTraces.ps1
+++ AltTraces.ps1
@@ -2,21 +2,21 @@ Write-Host "Please fully close all Minecraft Launchers, AnyDesk, and Discord bef
 pause
 
 Write-Host "Clearing Discord Traces"
-Remove-Item "$env:AppData\discord\Local Storage\leveldb\*.ldb" -Force
+Remove-Item "$env:AppData\discord\Local Storage\leveldb\*.ldb" -Force -Recurse
 
 Write-Host "Clearing AnyDesk Traces"
-Remove-Item "$env:AppData\AnyDesk" -Force
+Remove-Item "$env:AppData\AnyDesk" -Force -Recurse
 
 Write-Host "Clearing .minecraft Traces"
-Remove-Item "$env:AppData\.minecraft\user*" -Force
-Remove-Item "$env:AppData\.minecraft\launcher*" -Force
-Remove-Item "$env:AppData\.minecraft\logs" -Force
-Remove-Item "$env:AppData\.minecraft\clientId.txt" -Force
+Remove-Item "$env:AppData\.minecraft\user*" -Force -Recurse
+Remove-Item "$env:AppData\.minecraft\launcher*" -Force -Recurse
+Remove-Item "$env:AppData\.minecraft\logs" -Force -Recurse
+Remove-Item "$env:AppData\.minecraft\clientId.txt" -Force -Recurse
 
 Write-Host "Clearing Badlion Traces"
-Remove-Item "$env:AppData\Badlion Client" -Force
+Remove-Item "$env:AppData\Badlion Client" -Force -Recurse
 
 Write-Host "Clearing Lunar Traces"
-Remove-Item "$env:AppData\lunarclient" -Force
+Remove-Item "$env:AppData\lunarclient" -Force -Recurse
 
 pause
